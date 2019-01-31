002f089d772d72b703bc847b79f26f1db4d2bc81
Zhiyuan Li <zhiyuanli@huntsman-ve506-0329.apn.wlan.upenn.edu>
2019-01-31 13:17:19 -0500

    MongoDB Query

--- src/db/DBConnectionFactory.java
+++ src/db/DBConnectionFactory.java
@@ -1,17 +1,19 @@
 package db;
 
+import db.mongodb.MongoDBConnection;
 import db.mysql.MySQLConnection;
 
 public class DBConnectionFactory {
 
-	private static final String DEFAULT_DB = "mysql";
+	// This should change based on the pipeline.
+	private static final String DEFAULT_DB = "mongodb";
 
 	public static DBConnection getDBConnection(String db) {
 		switch (db) {
 		case "mysql":
 			return new MySQLConnection(); // return new MySQLConnection();
 		case "mongodb":
-			return null; // return new MongoDBConnection();
+			return new MongoDBConnection(); // return new MongoDBConnection();
 		default:
 			throw new IllegalArgumentException("Invalid db" + db);
 		}
